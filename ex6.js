dab6a3d338521199c81b65b9706989b31621083f
kasperdiem <153782572+kasperdiem@users.noreply.github.com>
2024-02-14 19:01:02 -0300

    EXERCÍCIO 6

---
+++ ex6.js
@@ -0,0 +1,24 @@
+const cao1 = {
+    nome: 'Cookie',
+    idade: '13',
+    carcterística: 'Resmungão',
+    brinquedo_favorito: 'Bola'
+}
+
+const cao2 = {
+    nome: 'Gabriela (Gabi)',
+    idade: '4',
+    carcterística: 'Carente',
+    brinquedo_favorito: 'Amigurumi de Baleia'
+}
+
+const cao3 = {
+    nome: 'Kyara',
+    idade: '2',
+    carcterística: 'Medrosa',
+    brinquedo_favorito: 'Garrafa de água'
+}
+
+console.log(cao1)
+console.log(cao2)
+console.log(cao3)
